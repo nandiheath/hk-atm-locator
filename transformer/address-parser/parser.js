c3ab36cac27a82d755a709f756d03396f24de43c
Nandiheath <nandi0315@gmail.com>
2019-05-07 21:59:10 +0800

    speed up the parser with parallel process

--- transformer/address-parser/parser.js
+++ transformer/address-parser/parser.js
@@ -4,7 +4,7 @@ const program = require('commander');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const AddressParser = require('hk-address-parser-lib');
-
+const async = require('async');
 // default logger
 const { log } = console;
 
@@ -15,21 +15,29 @@ function end() {
   process.exit(1);
 }
 
+async function parseAddress(atm) {
+  const addressLine = atm.ATMAddress.AddressLine.join(' ');
+  const records = await AddressParser.parse(addressLine);
+  if (records.length > 0) {
+    atm.Location = records[0].coordinate();
+  }
+}
+
 async function processFile(inputFile, outputFile, options) {
   const input = await fs.readFileAsync(inputFile);
   const data = JSON.parse(input.toString());
   const atms = data.data[0].Brand[0].ATM;
-  for (const atm of atms) {
-    const address = `${atm.ATMName}`;
-    const addressLine = atm.ATMAddress.AddressLine.join(' ');
-    const records = await AddressParser.parse(addressLine);
-    if (records.length > 0) {
-      atm.Location = records[0].coordinate();
-    }
-  }
 
-  await fs.writeFileAsync(outputFile, JSON.stringify(data, null, 4));
-  end();
+  log(`Start to process the data. Total: ${atms.length}`);
+
+  async.eachOfLimit(atms, 50, async.asyncify(parseAddress), async (err) => {
+    log('Process finished');
+    await fs.writeFileAsync(outputFile, JSON.stringify(data, null, 4));
+    end();
+  });
+
+
+
 }
 
 
