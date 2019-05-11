0374597db3250a3c7ec7889b4d4ebec3f400e194
Nandiheath <nandi0315@gmail.com>
2019-05-11 15:22:30 +0800

    update travis

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -7,13 +7,13 @@ setup_git() {
 }
 
 commit_country_json_files() {
-  git checkout -b master
+  git checkout master
 
   git status
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
   # Stage the modified files in dist/output
-  git add -f web/src/data/*.json
+  git add -f web/src/data/
   # Create a new commit with a custom build message
   # with "[skip ci]" to avoid a build loop
   # and Travis build number for reference
